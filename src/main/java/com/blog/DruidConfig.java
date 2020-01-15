01983051d41d57767d722bf2a734f8e05c0144b0
邓卫平 <dengweiping@sinobest.cn>
2020-01-15 15:32:46 +0800

    项目初始化

--- src/main/java/com/blog/DruidConfig.java
+++ src/main/java/com/blog/DruidConfig.java
@@ -13,29 +13,29 @@ import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 public class DruidConfig {
-//    @Bean
-//    public ServletRegistrationBean druidServlet() {// 主要实现web监控的配置处理
-//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
-//            new StatViewServlet(), "/druid/*");//表示进行druid监控的配置处理操作
-//        servletRegistrationBean.addInitParameter("allow", "127.0.0.1,129.168.1.11");//白名单
-//        servletRegistrationBean.addInitParameter("deny", "129.168.1.12");//黑名单
-//        servletRegistrationBean.addInitParameter("loginUsername", "root");//用户名
-//        servletRegistrationBean.addInitParameter("loginPassword", "root");//密码
-//        servletRegistrationBean.addInitParameter("resetEnable", "false");//是否可以重置数据源
-//        return servletRegistrationBean;
-//
-//    }
-//    @Bean    //监控
-//    public FilterRegistrationBean filterRegistrationBean(){
-//        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean();
-//        filterRegistrationBean.setFilter(new WebStatFilter());
-//        filterRegistrationBean.addUrlPatterns("/*");//所有请求进行监控处理
-//        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.css,/druid/*");//排除
-//        return filterRegistrationBean;
-//    }
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource druidDataSource() {
-//        return new DruidDataSource();
-//    }
+    @Bean
+    public ServletRegistrationBean druidServlet() {// 主要实现web监控的配置处理
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+            new StatViewServlet(), "/druid/*");//表示进行druid监控的配置处理操作
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1,129.168.1.11");//白名单
+        servletRegistrationBean.addInitParameter("deny", "129.168.1.12");//黑名单
+        servletRegistrationBean.addInitParameter("loginUsername", "root");//用户名
+        servletRegistrationBean.addInitParameter("loginPassword", "root");//密码
+        servletRegistrationBean.addInitParameter("resetEnable", "false");//是否可以重置数据源
+        return servletRegistrationBean;
+
+    }
+    @Bean    //监控
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean filterRegistrationBean=new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");//所有请求进行监控处理
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.css,/druid/*");//排除
+        return filterRegistrationBean;
+    }
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
 }
