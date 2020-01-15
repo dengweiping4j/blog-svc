5deef0b006f35a31511c628e8dadfa444d8a7bbf
邓卫平 <dengweiping@sinobest.cn>
2020-01-15 15:58:29 +0800

    项目初始化

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -3,11 +3,15 @@ package com.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dengweiping
+ * 2020-01-15 15:54
+ */
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
 
 }
